1058bf5af6545f478deae38ac93aeb5bffd89405
Adam Forrester <adam@grapedesignllc.com>
2015-12-30 14:47:47 -0600

    Checkpoint-3 Completed

--- dist/scripts/services/timer.js
+++ dist/scripts/services/timer.js
@@ -7,17 +7,53 @@ blocPomodoro.directive('timer', ['$interval', function($interval) {
         link: function(scope, element, attributes) {
             
             scope.timeLeft = "25:00";
-            scope.showStartButton = true;
+            scope.showWorkStartButton = true;
+            scope.showBreakStartButton = false;
             
-            var totalTimeInSeconds = 1500;
             var promise;
             
-            scope.startTimer = function() {
-                console.log('I started');
+            /* Starts the Timer and takes in length of timer */
+            scope.startTimer = function(totalTimeInSeconds) {
+                    
+                if (scope.showBreakStartButton == true) {
+                        console.log('Show Break Reset Button');
+                        scope.showBreakStartButton = false;
+                        scope.showBreakResetButton = true;
+                }
                 
-                promise = $interval(reduceByOneSecond, 1000);
-                scope.showStartButton = false;
-                scope.showResetButton = true;
+                else if (scope.showWorkStartButton == true) {
+                        console.log('Show Work Reset Button');
+                        scope.showWorkStartButton = false;
+                        scope.showWorkResetButton = true;
+                }
+                
+                /* starts a function that reduces time by 1 second every second */
+                /* takes in argument for the time to reduce */ 
+                promise = $interval(function() {
+                    
+                    totalTimeInSeconds--;
+                    console.log(totalTimeInSeconds);
+                    
+                    var minutes = Math.floor(totalTimeInSeconds % 3600 / 60);
+                    var seconds = Math.floor(totalTimeInSeconds % 3600 % 60);
+
+                    if (minutes < 10) {minutes = "0"+minutes;}
+                    if (seconds < 10) {seconds = "0"+seconds;}
+                    scope.timeLeft = minutes+':'+seconds;
+                    
+                    /* If time gets to zero and it's not on a break then */ 
+                    /* resets timer to 5 minutes  */ 
+                    if (totalTimeInSeconds == 0 && scope.showBreakStartButton == false) {
+                        scope.resetTimer(300);
+                        
+                    /* If time gets to zero and it is on a break then */ 
+                    /* resets timer to 25 minutes */ 
+                    } else if (totalTimeInSeconds == 0 && scope.showBreakStartButton == true) {
+                        scope.resetTimer(1500);
+                    }
+
+                }, 1000);
+                
             };
             
             
@@ -26,27 +62,35 @@ blocPomodoro.directive('timer', ['$interval', function($interval) {
             };
             
             
-            scope.resetTimer = function() {
+            scope.resetTimer = function(totalTimeInSeconds) {
                 console.log('I reset');
                 scope.stopTimer();
-                totalTimeInSeconds = 1500;
-                scope.showStartButton = true;
-                scope.showResetButton = false;
+                scope.timeLeft = scope.convertSecondsToTime(totalTimeInSeconds);
+                
+                if (totalTimeInSeconds == 300) {
+                    scope.showBreakStartButton = true;
+                    scope.showBreakResetButton = false;
+                    scope.showWorkStartButton = false;
+                    scope.showWorkResetButton = false;
+                }
+                else if (totalTimeInSeconds == 1500) {
+                    scope.showBreakStartButton = false;
+                    scope.showBreakResetButton = false;
+                    scope.showWorkStartButton = true;
+                    scope.showWorkResetButton = false;
+                }
             };
             
-            
-            var reduceByOneSecond = function() {
-                totalTimeInSeconds--;
-                console.log(totalTimeInSeconds);
-                
+            scope.convertSecondsToTime = function(totalTimeInSeconds) {
                 var minutes = Math.floor(totalTimeInSeconds % 3600 / 60);
                 var seconds = Math.floor(totalTimeInSeconds % 3600 % 60);
                 
                 if (minutes < 10) {minutes = "0"+minutes;}
                 if (seconds < 10) {seconds = "0"+seconds;}
-                scope.timeLeft = minutes+':'+seconds;
+                return scope.timeLeft = minutes+':'+seconds;
             };
+            
         }
-    }  
+    };
 
 }]);
